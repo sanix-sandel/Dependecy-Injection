bf8a93f111ea29c10eb85ce9254840f3bbc629b9
sanix sandel <sanicksikani@gmail.com>
2020-08-13 19:43:37 +0300

    Profile

---
+++ src/main/java/sanix/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,16 @@
+package sanix.springframework.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting(){
+        return "Servicio de Saludo Primaro";
+    }
+}
